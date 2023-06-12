e504dce216f628d47e7a767b8fa5c90293272f3a
Kosuke Takeu <n.kosuke0906@gmail.come>
2023-06-13 00:41:42 +0900

    style トップページのスタイリング修正

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,11 @@ const Home = async () => {
       <h1 className="pageTitle" style={{ textAlign: 'center' }}>
         都道府県別人口構成グラフ
       </h1>
-      <p className="pageDescription"></p>
+      <p className="pageDescription" style={{ textAlign: 'center' }}>
+        人口構成の推移をグラフで表示します。都道府県を選択してください。
+        <br />
+        「総人口」、「年少人口」、「生産年齢人口」、「老年人口」の4種類のデータを表示できます。
+      </p>
       <Chart prefectures={prefectures} />
     </main>
   );
